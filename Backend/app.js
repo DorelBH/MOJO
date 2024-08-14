70477cae2a08979b9eff47848008e06a99bf4e36
ronmo4 <ronmo4@ac.sce.ac.il>
2024-08-14 17:04:23 +0300

    try to fix host

--- Backend/app.js
+++ Backend/app.js
@@ -15,10 +15,26 @@ app.use('/api/events', eventsRoutes); // => api/events/....
 
 const PORT = process.env.PORT || 3500; // השתמש במשתנה הסביבה PORT או ב-3500 כברירת מחדל
 
+// בדיקה אם האפליקציה רצה על Render
+const isRender = process.env.RENDER || false; // Render מגדירה משתנה סביבה בשם RENDER
+
+app.get('/check-host', (req, res) => {
+    if (isRender) {
+        res.json({ message: `App is running on Render at ${req.hostname}` });
+    } else {
+        res.json({ message: `App is running locally at ${req.hostname}:${PORT}` });
+    }
+});
+
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+      if (isRender) {
+          console.log(`App is running on Render at ${PORT}`);
+      } else {
+          console.log(`App is running locally at ${PORT}`);
+      }
     });
   })
   .catch(err => {
